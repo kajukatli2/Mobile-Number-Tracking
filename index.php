4454d88e0597f96594ed224b6b297c69d9c6a351
kajukatli2 <112706450+kajukatli2@users.noreply.github.com>
2022-09-15 20:10:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php
-/******
- * Will be updated soon.
- * ****/
+<!doctype html>
+<html>
+	<head><title>Mobile Number Trakcing</title></head>
+	<body><p>Use the form below to track any mobile number details.</p><form method="get" action="https://calltracer.org/"><input type="text" name="q" placeholder="Enter Mobile Number to Track"><input type="submit" name="button" value="Track"></form></body>
+</html>
